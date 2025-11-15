72f05e42b8522c3a335855839596eb6900a19c6f
ucycg <ucycg@studen.kit.edu>
2025-11-15 17:33:03 +0100

    Create Basic Boid Class

---
+++ ConsoleBoids/ConsoleBoids/Boid.cs
@@ -0,0 +1,28 @@
+﻿namespace ConsoleBoids;
+
+public class Boid
+{
+    internal char Symbol { get; set; }
+    private float X { get; set; }
+    private float Y { get; set; }
+    
+    private float Dx { get; set; }
+    private float Dy { get; set; }
+    
+    private float Speed { get; set; }
+
+    public (float, float) CalculateNewPosition()
+    {
+        return (CalculateNewXPosition(), CalculateNewYPosition());
+    }
+
+    private float CalculateNewYPosition()
+    {
+        return this.Y + this.Dy * this.Speed;
+    }
+
+    private float CalculateNewXPosition()
+    {
+        return this.X + this.Dx * this.Speed;
+    }
+}
